4376bd82f754218975e01b437b3b339837b23411
Eacarr1992 <90227363+Eacarr1992@users.noreply.github.com>
2021-10-08 15:46:11 -0400

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,38 @@
+#print a string
+print("Mary had a little lamb.")
+#print a string with a formatted {} including snow
+print("Its fleece was white as {}.".format('snow'))
+#print a string
+print("And everywhere that Mary went.")
+#print a period multiplied by 10
+print("." * 10)
+
+#assign a letter as a string variable
+end1 = "C"
+#assign a letter as a string variable
+end2 = "h"
+#assign a letter as a string variable
+end3 = "e"
+#assign a letter as a string variable
+end4 = "e"
+#assign a letter as a string variable
+end5 = "s"
+#assign a letter as a string variable
+end6 = "e"
+#assign a letter as a string variable
+end7 = "B"
+#assign a letter as a string variable
+end8 = "u"
+#assign a letter as a string variable
+end9 = "r"
+#assign a letter as a string variable
+end10 = "g"
+#assign a letter as a string variable
+end11 = "e"
+#assign a letter as a string variable
+end12 = "r"
+
+#print variables end1 - end 6 and end the line with a space
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+#print variables end7 - end12
+print(end7 + end8 + end9 + end10 + end11 + end12)
