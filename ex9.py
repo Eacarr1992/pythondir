dde82aa6891feae0d35088e5cb037c03e16a12ae
Eacarr1992 <90227363+Eacarr1992@users.noreply.github.com>
2021-10-09 10:21:59 -0400

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,16 @@
+#assign a string to a variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#assign a string with each \n starting a new line to a variable
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+#print a string and a variable outside of the quotation marks
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+#print a string, using triple double quotes we are able to print separate lines, instead of one long line like in ex8
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
